18bbbc6d878d4d0aabbe094ffc3b876789e31492
Guilherme Lima <ghduep@outlook.com>
2025-11-18 11:43:04 -0300

    finishing sendemailnotification function

--- mail.go
+++ mail.go
@@ -1,21 +1,25 @@
 package main
 
 import (
+	"log"
 	"net/smtp"
 )
 
-func sendEmailNotification(website Website, status string){
-	auth := smtp.PlainAuth()
+func main() {
+	sendEmailNotification("https://example.com", "UP")
+}
 
-				to := []string{"ghduep@outlook.com"}
-				msg := []byte("To: ghduep@outlook.com\r\n" +
-					"Subject: Your server status has changed\r\n" +
-					"\r\n" +
-					"The status of " + website.URL + " has changed to " + status + ".\r\n")
-			}
+func sendEmailNotification(url string, status string) {
+	auth := smtp.PlainAuth("", "ghduuep@gmail.com", "krve whaq yzpi jwcq", "smtp.gmail.com")
 
-			err := smtp.SendMail("smtp.dominio:587", auth, from, to, msg)
-			if err != nil {
-				log.Fatal(err)
-			}
+	to := []string{"ghduep@outlook.com"}
+	msg := []byte("To: ghduep@outlook.com\r\n" +
+		"Subject: Your server status has changed\r\n" +
+		"\r\n" +
+		"The status of " + url + " has changed to " + status + ".\r\n")
+
+	err := smtp.SendMail("smtp.gmail.com:587", auth, "ghduep@outlook.com", to, msg)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
