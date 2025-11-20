8a089b08e92cedd951ea57c1041d79e8c08a43a4
Guilherme Lima <ghduep@outlook.com>
2025-11-20 13:10:21 -0300

    changing check function logic

--- internal/monitor/monitor.go
+++ internal/monitor/monitor.go
@@ -2,11 +2,9 @@ package monitor
 
 import (
 	"context"
-	"log"
 	"net/http"
 	"time"
 
-	"github.com/ghduuep/pingly/internal/database"
 	"github.com/ghduuep/pingly/models"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -18,7 +16,7 @@ func CheckSite(url string) (string, error) {
 
 	resp, err := client.Get(url)
 	if err != nil {
-		return resp.Status, err
+		return "DOWN", err
 	}
 	defer resp.Body.Close()
 
@@ -26,16 +24,4 @@ func CheckSite(url string) (string, error) {
 }
 
 func startMonitoring(ctx context.Context, db *pgxpool.Pool, sites []*models.Website) {
-	for _, site := range sites {
-		status, err := CheckSite(site.URL)
-		if err != nil {
-			log.Printf("Cannot connect to website: %v", err)
-		}
-		if status != site.LastStatus && status != "UNKNOWN" {
-			log.Printf("Status changed for %s: %s -> %s", site.URL, site.LastStatus, status)
-			if err := database.UpdateWebsiteStatus(ctx, db, site.ID, status); err != nil {
-				log.Printf("Error updating website status: %v", err)
-			}
-		}
-	}
 }
