324096a42694449ce1443858eb8d3759b3e7e29a
Guilherme Lima <ghduep@outlook.com>
2025-11-21 12:00:44 -0300

    making credencials on notification secret on .env

--- internal/notification/notification.go
+++ internal/notification/notification.go
@@ -1,11 +1,19 @@
 package notification
 
 import (
+	"log"
 	"net/smtp"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func SendEmailNotification(userEmail string, url string, status string) error {
-	auth := smtp.PlainAuth("", "ghduuep@gmail.com", "krve whaq yzpi jwcq", "smtp.gmail.com")
+	if err := godotenv.Load(); err != nil {
+		log.Println("Cannot load .env file.")
+	}
+
+	auth := smtp.PlainAuth("", os.Getenv("EMAIL_SENDER"), os.Getenv("EMAIL_PASSWORD"), os.Getenv("EMAIL_SMTP_HOST"))
 
 	to := []string{userEmail}
 	msg := []byte("To:" + userEmail + "\r\n" +
@@ -13,5 +21,5 @@ func SendEmailNotification(userEmail string, url string, status string) error {
 		"\r\n" +
 		"The status of " + url + " has changed to " + status + ".\r\n")
 
-	return smtp.SendMail("smtp.gmail.com:587", auth, "ghduuep@gmail.com", to, msg)
+	return smtp.SendMail(os.Getenv("EMAIL_SMTP_HOST"+":"+os.Getenv("EMAIL_SMTP_PORT")), auth, os.Getenv("EMAIL_SENDER"), to, msg)
 }
