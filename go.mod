2a0c28156250975a11476a8079c7a0fd1b9e4e34
Guilherme Lima <ghduep@outlook.com>
2025-11-23 07:54:44 -0300

    feat: implemeting routing with chi

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ghduuep/pingly
 go 1.25.3
 
 require (
+	github.com/go-chi/chi/v5 v5.2.3
 	github.com/jackc/pgx/v5 v5.7.6
 	github.com/joho/godotenv v1.5.1
 )
--- internal/api/routes/routes.go
+++ internal/api/routes/routes.go
@@ -1,23 +1,28 @@
 package routes
 
 import (
-	"net/http"
-
 	"github.com/ghduuep/pingly/internal/api/handlers"
 	"github.com/ghduuep/pingly/internal/models"
 	"github.com/jackc/pgx/v5/pgxpool"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
-func NewRouter(db *pgxpool.Pool, siteChan chan *models.Website) *http.ServeMux {
+func NewRouter(db *pgxpool.Pool, siteChan chan *models.Website) *chi.Mux {
 	h := &handlers.Handler{
 		DB:           db,
 		NewSitesChan: siteChan,
 	}
 
-	mux := http.NewServeMux()
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
-	mux.HandleFunc("POST /websites", h.CreateWebsite)
-	mux.HandleFunc("GET /websites", h.GetAllWebsites)
+	r.Group(func(r chi.Router) {
+		r.Get("/websites", h.GetAllWebsites)
+		r.Post("/websites", h.CreateWebsite)
+	})
 
-	return mux
+	return r
 }
