942d928d6d817eb004f71f2b087100f04b0c9f26
Guilherme Lima <ghduep@outlook.com>
2025-11-24 17:57:07 -0300

    feat: creating DNS records models

--- internal/models/models.go
+++ internal/models/models.go
@@ -26,3 +26,16 @@ type CheckLog struct {
 	Status    string    `json:"status"`
 	CreatedAt time.Time `json:"created_at"`
 }
+
+type DNSRecords struct {
+	A  []string `json:"a_records"`
+	MX []string `json:"mx_records"`
+	NS []string `json:"ns_records"`
+}
+
+type DNSState struct {
+	ID          int        `json:"id"`
+	WebsiteID   int        `json:"website_id"`
+	Records     DNSRecords `json:"records"`
+	LastChecked time.Time  `json:"checked_at"`
+}
