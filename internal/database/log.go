44b382d1a8de7717673d0887fd5e934987dc7454
Guilherme Lima <ghduep@outlook.com>
2025-11-25 14:59:05 -0300

    fix: fixing SQL code

--- internal/database/log.go
+++ internal/database/log.go
@@ -7,7 +7,7 @@ import (
 )
 
 func CreateUptimeLog(ctx context.Context, db *pgxpool.Pool, websiteID int, status string, rootCause string) error {
-	query := `INSERT INTO uptime_logs (website_id, status, root_cause) VALUES ($1, $2, 3$)`
+	query := `INSERT INTO uptime_logs (website_id, status, root_cause) VALUES ($1, $2, $3)`
 
 	_, err := db.Exec(ctx, query, websiteID, status, rootCause)
 
